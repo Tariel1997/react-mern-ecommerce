2e8caeb348e516c6c681be154f9857a1606eab8f
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2026-02-10 17:38:05 +0400

    feat: implement signup with JWT tokens and Redis storage

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 import dotenv from 'dotenv'
 import express from 'express'
 
+import { connectDB } from './lib/db.js'
 import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
+
+app.use(express.json())
 
 app.use('/api/auth', authRoutes)
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT)
+
+  connectDB()
 })
