09934f7f26d0ba865c98c3c13efbfe15e208cb9f
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2026-02-13 19:08:44 +0400

    feat: implement product model, controllers, and admin/protect middleware

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,40 @@
+import { redis } from '../lib/redis.js'
+import Product from '../models/product.model.js'
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({})
+    res.status(200).json({ products })
+  } catch (error) {
+    console.log('Error in getAllProducts controller: ', error.message)
+    res
+      .status(500)
+      .json({ message: 'Internal server error', error: error.message })
+  }
+}
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get('featured_products')
+    if (featuredProducts) {
+      return res.status(200).json(JSON.parse(featuredProducts))
+    }
+
+    // If not in Redis, fetch from MongoDB
+    featuredProducts = await Product.find({ isFeatured: true }).lean()
+
+    if (!featuredProducts) {
+      return res.status(404).json({ message: 'No featured products found' })
+    }
+
+    // Store in Redis for future quick access
+    await redis.set('featured_products', JSON.stringify(featuredProducts))
+
+    res.json(featuredProducts)
+  } catch (error) {
+    console.log('Error in getFeaturedProducts controller: ', error.message)
+    res
+      .status(500)
+      .json({ message: 'Internal server error', error: error.message })
+  }
+}
