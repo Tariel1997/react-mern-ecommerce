0af876122ff0ca734d51e0b86191ee0457d36826
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2026-02-14 17:17:25 +0400

    feat: implement coupon model and validation logic

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import express from 'express'
 import { connectDB } from './lib/db.js'
 import authRoutes from './routes/auth.route.js'
 import cartRoutes from './routes/cart.route.js'
+import couponRoutes from './routes/coupon.route.js'
 import productRoutes from './routes/product.route.js'
 
 dotenv.config()
@@ -18,6 +19,7 @@ app.use(cookieParser())
 app.use('/api/auth', authRoutes)
 app.use('/api/products', productRoutes)
 app.use('/api/cart', cartRoutes)
+app.use('/api/coupons', couponRoutes)
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT)
