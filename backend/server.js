9a04197e284b3ffe74ea29ae34b3a0c254512a66
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2026-02-14 16:53:10 +0400

    feat: add cart routes and controllers for managing user cart items and quantities

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import express from 'express'
 
 import { connectDB } from './lib/db.js'
 import authRoutes from './routes/auth.route.js'
+import cartRoutes from './routes/cart.route.js'
 import productRoutes from './routes/product.route.js'
 
 dotenv.config()
@@ -16,6 +17,7 @@ app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/products', productRoutes)
+app.use('/api/cart', cartRoutes)
 
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT)
