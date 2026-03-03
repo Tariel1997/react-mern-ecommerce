d280c5625ef83797ded4785c049ed010cb9a20e9
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2026-03-03 16:37:24 +0400

    feat: add OrderSummary component with smooth animations and responsive UI

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -4,6 +4,8 @@ import { useState } from 'react'
 import CartItem from '../components/CartItem'
 import ConfirmModal from '../components/ConfirmModal'
 import EmptyCartUI from '../components/EmptyCartUI'
+import GiftCouponCard from '../components/GiftCouponCard'
+import OrderSummary from '../components/OrderSummary'
 import PeopleAlsoBought from '../components/PeopleAlsoBought'
 import { useCartStore } from '../stores/useCartStore'
 
@@ -36,6 +38,18 @@ const CartPage = () => {
             )}
             {cart.length > 0 && <PeopleAlsoBought />}
           </motion.div>
+
+          {cart.length > 0 && (
+            <motion.div
+              className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full"
+              initial={{ opacity: 0, x: 20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+            >
+              <OrderSummary />
+              <GiftCouponCard />
+            </motion.div>
+          )}
         </div>
       </div>
 
